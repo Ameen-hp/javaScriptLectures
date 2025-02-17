62f87c262de60af617300d21adb08191ea04d70b
Ameen-hp <devameen45@gmail.com>
2025-02-17 08:28:57 -0800

    for loop

---
+++ loops.js
@@ -0,0 +1,41 @@
+// loops ??
+// for,while,do while
+// Syntax;
+
+// for(intialization,checkCondition,increment){
+        
+//     // thing to print
+// }
+
+// //example #
+
+// for(let i = 1;i <= 6;i++){
+//    console.log("you are student");
+// }
+
+// you are student
+// you are student
+// you are student
+// you are student
+// you are student
+// you are student
+
+
+// table program
+
+// for (let i = 1; i <= 10; i +){
+//     console.log(`${i}*9 =  ${i * 9}`);    
+// }
+
+
+
+
+//even numbers sum till 100
+// let sum = 0
+// for (let i = 1; i <= 100; i++){
+//     if (i % 2 == 0) {
+//         sum = sum + i;
+//   }  
+// }
+
+// console.log(sum);
