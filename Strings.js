9c1b8f04d531e5f2c03d5cc6658bfe048659cea7
Ameen-hp <devameen45@gmail.com>
2025-02-20 08:27:22 -0800

    String and methods

---
+++ Strings.js
@@ -0,0 +1,27 @@
+// do while loop,whlile loop, for loop
+
+// Strings
+
+
+// let store = " Muhuammad Ameen \n YougeshKumar";  //Strings
+
+// length
+// console.log(store.length);
+// console.log(store);
+
+
+
+// String Methods
+
+let Name = "JavaScript is the programming language";
+let Name2 = "python is the programming language";
+let Name3 = "c++ is the progarmming language";
+// variable.Method()
+console.log(Name.charAt(2)); // to find the character from the given position
+console.log(Name[2]);
+console.log(Name.slice(10, 20))
+console.log(Name.substring(10, 20));
+console.log(Name.substr(10, Name.length));
+console.log(Name.toUpperCase())
+console.log(Name.toLowerCase());
+console.log(Name3.concat(" "+Name," "+Name2));
