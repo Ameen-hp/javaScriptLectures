b9930d874058d1705fa99d6e767ac39d97fdd04c
Ameen-hp <devameen45@gmail.com>
2025-03-03 10:10:16 -0800

    Basic of Arrays

---
+++ Array.js
@@ -0,0 +1,57 @@
+
+// Arrays
+// vehicles data
+// truck,bike,car,darsan,mazda,riksha,
+
+
+// let vehicles = ["truck", "car", "bike", "darsan", "riksha", "trailor"];
+
+// for (let i = 0; i < vehicles.length; i++) {
+//     console.log(vehicles[i])
+// }
+// truck
+//car
+//bike
+// darsan
+// riksha
+// trailor
+
+// print the bike variable if available
+
+// let vehicles = ["truck", "car", "bike", "darsan", "riksha", "trailor"];
+// let hint = false;
+// for (let i = 0; i < vehicles.length; i++) {
+//     if (vehicles[i] == "bycicle") {
+//         hint = true;
+//     }
+    
+// }
+
+// if (hint) {
+//     console.log("the element u have entered is available in this array")
+// }
+// else {
+//     console.log("the vairable is not available")
+// }
+
+
+
+// sum of even numbers
+
+
+// let Numbers = [2, 4, 6, 5, 14, 13, 13, 1, 3, 31];
+// let sum = 0
+// for (let i = 0; i < Numbers.length; i++) {
+//     if (Numbers[i] % 2 == 0) {
+//         sum = sum+Numbers[i]
+//     }
+// }
+
+// console.log(sum);
+ 
+
+
+// different types of data in the array
+
+let car = ["corola", "F41-123", 2020, "blac", 2000000];
+console.log(car);
