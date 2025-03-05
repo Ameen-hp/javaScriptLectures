f6d882e2693d171075703890cab65ed2602eab4e
Ameen-hp <devameen45@gmail.com>
2025-03-05 10:10:36 -0800

    nested loops,break,continue, 2d array

--- Array.js
+++ Array.js
@@ -1,57 +1,74 @@
 
-// Arrays
-// vehicles data
-// truck,bike,car,darsan,mazda,riksha,
-
-
-// let vehicles = ["truck", "car", "bike", "darsan", "riksha", "trailor"];
-
-// for (let i = 0; i < vehicles.length; i++) {
-//     console.log(vehicles[i])
+// nested for loop
+// outer loop
+// for (let i = 1; i < 5;i++) {
+//     console.log(i);
+//     for (let j = 1; j <= 3; j++) {
+//         console.log("javaScript");
+//     } // inner loop
 // }
-// truck
-//car
-//bike
-// darsan
-// riksha
-// trailor
 
-// print the bike variable if available
+// 1
+// javaScript
+// javaScript
+// javaScript
+//2
+// javaScript
+// javaScript
+// javaScript
+//3
+ //javaScript
+// javaScript
+// javaScript
 
-// let vehicles = ["truck", "car", "bike", "darsan", "riksha", "trailor"];
-// let hint = false;
-// for (let i = 0; i < vehicles.length; i++) {
-//     if (vehicles[i] == "bycicle") {
-//         hint = true;
-//     }
+//4
+//  javaScript
+// javaScript
+// javaScript
+
+
+
+// break,continue
+
+// for (let i = 0; i < 10; i++) {
     
-// }
-
-// if (hint) {
-//     console.log("the element u have entered is available in this array")
-// }
-// else {
-//     console.log("the vairable is not available")
-// }
-
-
-
-// sum of even numbers
-
-
-// let Numbers = [2, 4, 6, 5, 14, 13, 13, 1, 3, 31];
-// let sum = 0
-// for (let i = 0; i < Numbers.length; i++) {
-//     if (Numbers[i] % 2 == 0) {
-//         sum = sum+Numbers[i]
+//     if (i == 3) {
+//         break;
+//     }
+//     else {
+//         console.log(i)
 //     }
 // }
 
-// console.log(sum);
- 
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("You are software engineer");
+//     for (let j = 0; j < 3; j++) {
+//         if (j == 2) {
+//             continue;
+//         }
+//         else {
+//             console.log("you are developer");
+//         }
+//     }
+// }
 
 
-// different types of data in the array
+// 2d array
 
-let car = ["corola", "F41-123", 2020, "blac", 2000000];
-console.log(car);
+// let numbers = [[[21,12,21],"banana","orange"], [2, 4, 6], [12, 10]];
+// console.log(numbers[0][0][1]);
+
+
+
+
+// let departments = [["software", "Computer Sceince", "Ai"], ["BBA", "Social sceince"], ["physics", "english", "Mathematics"]];
+
+// for (let i = 0; i < departments.length; i++) {
+    
+//     for (let j = 0; j < departments[i].length; j++) {
+//         console.log(departments[i][j]); 
+//     }
+// }
+
+
