7a67da7a31281943b2ed72da4ada65e2e81261ef
Ameen-hp <devameen45@gmail.com>
2025-03-06 10:02:41 -0800

    basic functions use and explaination of syntax and parameters,arguments,return learning

---
+++ function.js
@@ -0,0 +1,50 @@
+//functions
+// heart of javaScript
+// a piece/block of code for reusing
+
+// atm machine
+
+// 200 lines
+
+
+//
+
+// basic function defination and its syntax
+
+// function Hello() {
+  
+//     console.log("hello how are u?");
+
+// }
+
+// Hello();
+
+
+// function parameters and arguments
+
+// function sum(a,b) {   // parameters
+     
+//     console.log(a + b);
+// }
+
+// sum(50,60)   // arguments
+// sum()
+
+
+
+
+// return
+
+
+function sum() {
+    let a = 5;
+    let b = 6;
+    return a + b;
+}
+
+let store = sum() * 4;  /// 11
+console.log(store*3);
+
+
+
+
