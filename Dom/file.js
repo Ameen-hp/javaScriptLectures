b71e80495f60ae14ceb4e4fac8db90a9710c6815
Ameen-hp <devameen45@gmail.com>
2025-07-21 10:26:32 -0700

    select elements by class,id,tagName,querySelectorAll

---
+++ Dom/file.js
@@ -0,0 +1,48 @@
+// select an element by id 
+
+// let selectElement = document.getElementById("heading-one")
+// // selectElement.textContent = "welcome to javaScript"
+// // selectElement.innerHTML = "welcome to the javaScript"
+
+
+// console.log(selectElement)
+
+
+
+// select elements by className 
+
+// let selectElements = document.getElementsByClassName("h1");
+
+// // console.log(selectElements)
+// for(let i=0; i < selectElements.length;i++ ) {
+//     // console.log(selectElements[i])
+//     if(i==1) {
+//         continue
+//     }
+//     else{
+//         selectElements[i].innerHTML = "welcome to the javaScript Dom"
+//     }
+// }
+
+
+// select an elemeny by tagName
+
+// let selectElementByTagName = document.getElementsByTagName("p");
+
+//  for(let i = 0; i < selectElementByTagName.length;i++) {
+//     console.log(selectElementByTagName[i])
+//     selectElementByTagName[i].textContent = "hello hi am Ameen"
+//  }
+
+
+
+// query selector 
+
+// let selectELementByQuerySelector = document.querySelector(".h1");
+
+
+// querySelectorAll 
+
+let selectElementByQuerySelectorALl = document.querySelectorAll(".h1");
+console.log(selectElementByQuerySelectorALl)
+
