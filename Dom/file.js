2cf47f93d658aca0f8bb02927e5d119e80a88b12
Ameen-hp <devameen45@gmail.com>
2025-07-22 12:38:45 -0700

    Dom Lecture2

--- Dom/file.js
+++ Dom/file.js
@@ -43,6 +43,85 @@
 
 // querySelectorAll 
 
-let selectElementByQuerySelectorALl = document.querySelectorAll(".h1");
-console.log(selectElementByQuerySelectorALl)
+// let selectElementByQuerySelectorALl = document.querySelectorAll(".h1");
+// console.log(selectElementByQuerySelectorALl)
 
+
+
+// traversing elements
+
+// let selectParent = document.getElementById("parent")
+// // let selecltFirstChild = selectParent.firstElementChild.nextElementSibling.nextElementSibling
+// let selectLastElement = selectParent.lastElementChild.previousElementSibling.previousElementSibling
+
+// console.log(selectLastElement)
+
+
+// create element 
+
+// let createNewELement = document.createElement("h1")
+// createNewELement.textContent = "am element 1"
+// console.log(createNewELement)
+
+// let createNextElement = document.createElement("h2");
+// createNextElement.textContent="am element 2"
+
+
+// append element 
+// let selectUl  = document.getElementById("parent");
+// document.body.appendChild(createNewELement,createNextElement)
+// document.body.append(createNewELement,createNextElement)
+// console.log(document.body)
+
+
+// insert ==>   insert(element,position)
+// replace element
+
+
+// let selectParent  = document.getElementById("parent");
+// let position = selectParent.firstElementChild.nextElementSibling.nextElementSibling;
+
+// let element  = document.createElement("li");
+// element.textContent = "new element"
+
+// selectParent.replaceChild(element,position)
+
+
+
+
+// remove element 
+
+
+// let selectParent = document.getElementById("parent")
+// let selectSecondChild = selectParent.firstElementChild.nextElementSibling
+// console.log(selectSecondChild)
+
+
+// selectParent.removeChild(selectSecondChild)
+
+
+
+// copy and paste an element
+
+
+// let  selectParent = document.getElementById("parent");
+
+// let copyElement = selectParent.cloneNode(true)
+
+// console.log(copyElement)
+
+// let selectNextParent = document.getElementById("nextParent");
+// selectNextParent.appendChild(copyElement)
+
+// console.log(selectNextParent)
+
+
+
+// Insert Adjacent
+
+let selectDivElement = document.getElementById("div-element");
+
+
+selectDivElement.insertAdjacentHTML("afterend","<h1>welcome to javaScript</h1>");
+
+// console.log(document.body)
